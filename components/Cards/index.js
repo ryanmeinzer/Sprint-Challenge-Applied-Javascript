9cd45ba4b6309d4ff360c6586181084f442a0652
Ryan Meinzer <ryanmeinzer@ryanmeinzer.com>
2020-02-14 09:57:50 -0800

    Cards started just need to create function for DOM component

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -17,3 +17,34 @@
 // </div>
 //
 // Create a card for each of the articles and add the card to the DOM.
+
+axios
+.get("https://lambda-times-backend.herokuapp.com/articles")
+.then(response => {
+    console.log(response.data);
+// });
+    Object.values(response.data.articles).forEach(topic => {
+        topic.forEach(article => {
+        entryPoint.appendChild(Cards(article))
+        // or could use:
+        // const keys = Object.keys(response.data.articles);
+        // for (let index = 0; index < keys.length; index++) {
+        //     const key = keys[index];
+        //     response.data.articles[`${key}`].forEach(article => {
+        //         Cards(article);
+        //         entry.Point.appendChild(Cards(article));
+        //     });
+        // }
+        // or could use:
+        // const keys = Object.values(response.data.articles);
+        // keys.forEach(topic => {
+        //     topic.forEach(article => {
+        //         entryPoint.appendChild(Cards(article));
+        //     });
+        // });
+        })
+    })
+    })   
+.catch(error => {
+    console.log("No data here.", error);
+})
